c95e4062c3fe51f7d7c7d27354b6d005a37e6283
kairat y <k_y_2022@mail.ru>
2021-08-01 12:26:43 +0600

    packaged in plain struct

--- src/main/java/com/free/freevideostreamingapi/controller/VideoStreamingController.java
+++ src/main/java/com/free/freevideostreamingapi/controller/VideoStreamingController.java
@@ -6,10 +6,12 @@ import com.free.freevideostreamingapi.exception.ObjectNotFound;
 import com.free.freevideostreamingapi.repository.VideoFileRepository;
 import com.free.freevideostreamingapi.repository.VideoRepository;
 import org.apache.commons.io.FileUtils;
+import org.apache.commons.lang.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import javax.servlet.http.HttpServletResponse;
 import java.io.File;
 import java.io.IOException;
 import java.util.Optional;
@@ -36,7 +38,7 @@ public class VideoStreamingController {
     }
 
     @GetMapping("/api/v1/video-streaming/{id}/{filename}")
-    public ResponseEntity getVideoStreamingM3u8(@PathVariable String id, @PathVariable String filename) throws IOException {
+    public void getVideoStreamingM3u8(@PathVariable String id, @PathVariable String filename, HttpServletResponse httpServletResponse) throws IOException {
         Optional<VideoFile> videoOptional = videoFileRepository.findById(id);
         if (!videoOptional.isPresent()) {
             throw new ObjectNotFound();
@@ -44,8 +46,10 @@ public class VideoStreamingController {
 
         VideoFile videoFile = videoOptional.get();
         byte[] bytes = FileUtils.readFileToByteArray(new File(videoFile.getOutputDir()+"/"+videoFile.getId()+"/"+filename));
-
-        return ResponseEntity.ok().body(bytes);
+        if (StringUtils.contains(filename,".ts")){
+            httpServletResponse.addHeader("Content-Type", "video/mp2t");
+        }
+        httpServletResponse.getOutputStream().write(bytes);
     }
 
 }
